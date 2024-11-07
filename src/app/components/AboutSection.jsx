eecef3472d474f779dfdc72e44c97c319210f48f
MALIK MUDDASSIR SULTAN <muddassir.pc@gmail.com>
2024-11-07 19:05:40 +0530

    Add files via upload

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,101 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>Next Js</li>
+        <li>Spring Boot</li>
+        <li>JavaScript</li>
+        <li>React</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Bachelor of Techonology</li>
+        <li>Motilal Nehru National Institute of Techonology Allahabad</li>
+        <li>BS Data Science and Programming</li>
+        <li>Indian Institute of Techonology Madras</li>
+      </ul>  
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Google Cloud Introduction to Gen Ai Coursera</li>
+        <li>Google Data Analytics Professional Certificate Coursera</li>
+        <li>Spring Boot 3 Spring 6 and Hibernate for Beginners Udemy</li>
+        <li>AI for everyone deeplearning Ai</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            Highly motivated and detail-oriented software developer and Techonology analyst with a strong foundation in computer science.
+             Proficient in programming languages (C++, Java, Python, JavaScript) and development frameworks (Spring Boot, Node.js, React Js). 
+             Proven analytical and problem-solving skills, with experience in developing scalable solutions and driving business growth. Excellent communication and collaboration skills, with a passion for innovative technologies and continuous learning.
+
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
